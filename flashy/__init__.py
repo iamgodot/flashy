6014ba2af15769fb5ecf759465005acd9a99e01c
iamgodot <xugodot@gmail.com>
2021-10-06 23:16:43 +0800

    feat: initialize project

---
+++ flashy/__init__.py
@@ -0,0 +1,6 @@
+from .core import dict_subtract
+
+
+__all__ = [
+    'dict_subtract',
+]
---
+++ tests/test_dict_functions.py
@@ -0,0 +1,11 @@
+from flashy import dict_subtract
+
+
+class TestDictUtils():
+    '''Test dict related functions.
+    '''
+    def test_subtract(self):
+        dict1 = {'a': 11, 'b': 0}
+        dict2 = {'b': 0}
+
+        assert dict_subtract(dict1, dict2) == {'a': 11}
